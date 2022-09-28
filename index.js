7725d88b028fb5c4138312233e3df937bf87d6ae
ShaolinMatt999 <sparksmakesnoise@gmail.com>
2022-09-28 14:13:38 -0400

    some styling. conts of js. lots of errors so far

---
+++ index.js
@@ -0,0 +1,11 @@
+const ul_1 = document.querySelector(".op1");
+const ul_2 = document.querySelector(".op2");
+const ul_3 = document.querySelector(".op3");
+const ul_4 = document.querySelector(".op4");
+const ul_5 = document.querySelector(".op5");
+
+const q1 = document.querySelector(".q1")
+const q2 = document.querySelector(".q2")
+const q3 = document.querySelector(".q3")
+const q4 = document.querySelector(".q4")
+const q5 = document.querySelector(".q5")
